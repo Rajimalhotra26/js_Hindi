a77b22196c9e4e41c14ec4f75a961cc15bb2c172
Rajimalhotra26 <130722597+Rajimalhotra26@users.noreply.github.com>
2023-08-01 12:28:29 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId= 123;
+let accountEmail = "asd@gmail.com"
+var accountPassword = '1234';
+accountCity= "Jaipur";
+
+// accountId= 2;
+
+console.log(accountId);
+accountEmail = "mail.com"
+accountPassword = '5555';
+accountCity= "goa";
+let accountState;
+
+
+console.table([accountEmail,accountPassword,accountCity, accountState])
